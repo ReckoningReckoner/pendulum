828f5a98406793f53e39a523a46a1bba358e9f3c
Viraj Bangari <Viraj3f@gmail.com>
2017-02-22 16:19:39 -0500

    try forces

--- pendulum.py
+++ pendulum.py
@@ -1,45 +1,82 @@
 import numpy as np
+from numpy.linalg import norm
 from scipy.constants import g
+
+unit_i = np.array([1.0, 0.0])
+unit_j = np.array([0.0, 1.0])
+
+
+def angle_between(v1, v2):
+    costheta = np.dot(v1, v2)/norm(v1)/norm(v2)
+    return np.arccos(costheta)
 
 
 class Pendulum:
 
-    def __init__(self, length, initialXY, theta, mass):
+    def __init__(self, length, theta, mass):
         self.mass = mass
         self.theta = theta
         self.length = length
-        self.acceleration = 0  # Z component
-        self.velocity = 0      # Z component
+        self.acceleration = np.array([0.0, 0.0])  # Z component
+        self.velocity = np.array([0.0, 0.0])      # Z component
         self.I = self.mass * length ** 2
-        self.initialXY = initialXY
+        self.prev = None
+        self.next = None
 
-    def moment(self):
-        return np.array(self.length * np.cos(self.theta),
-                        self.length * np.sin(self.theta))-self.initialXY
+        self.moment =\
+            self.length * np.array([np.sin(self.theta), np.cos(self.theta)])
 
-    def tension(self):
-        moment = self.moment()
-        at = np.linalg.norm(self.velocity) ** 2/self.length
-        return at * -moment/np.linalg.norm(moment)
+    def attach_to(self, pendulum):
+        self.next = pendulum
+        pendulum.prev = self
 
     def gravity(self):
-        return np.array([0.0, self.mass * g])
+        return unit_j * self.mass * g
 
-    def add_forces(self, *forces):
-        net_torque = 0  # in Z
-        moment = self.moment()
-        for force in forces:
-            torque = np.cross(moment, force)
-            net_torque += torque
+    def update_moment(self):
+        self.moment =\
+            self.length * np.array([np.sin(self.theta), np.cos(self.theta)])
 
-        self.acceleration = net_torque/self.I
+    def tension(self):
+        tension_mag = self.mass * g * np.cos(self.theta)\
+                      - self.mass *\
+                      norm(self.velocity) ** 2/self.length
+
+        print("Grav", self.mass * g * np.cos(self.theta))
+        print("Tension", tension_mag)
+        print("Cent", self.mass * norm(self.velocity)**2 / self.length)
+        return tension_mag * -self.moment/norm(self.moment)
+
+    def update_acceleration(self, dt):
+        gravity = self.gravity()
+        tension = self.tension()
+        net_force = gravity + tension
+        self.acceleration = net_force/self.mass
 
     def update_velocity(self, dt):
         self.velocity += self.acceleration * dt
+        print("Velocity", norm(self.velocity))
 
     def update_position(self, dt):
-        self.theta += self.velocity * dt
+        self.moment += self.velocity * dt
+        self.moment *= self.length/norm(self.moment)
+        self.theta = angle_between(self.moment, unit_j)
+        if self.moment[0] < 0 and not self.theta < 0:
+            self.theta *= -1
+        elif self.moment[0] > 0 and not self.theta > 0:
+            self.theta *= -1
 
     def update_velocity_and_position(self, dt):
+        print("Old Moment", self.moment)
+        print("Old Theta", np.rad2deg(self.theta))
+        print("length/moment", self.length/norm(self.moment))
+
+        self.update_moment()
+        self.update_acceleration(dt)
         self.update_velocity(dt)
         self.update_position(dt)
+
+        print("New Moment", self.moment)
+        print("Moment X", self.moment[0])
+        print("Moment y", self.moment[1])
+        print("New Theta", np.rad2deg(self.theta))
--- run.py
+++ run.py
@@ -1,20 +1,33 @@
 from pendulum import Pendulum
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+from math import pi
 
-simple = Pendulum(1, np.array([0, 0]), np.pi/3, 10)
+p1 = Pendulum(1, np.deg2rad(10), 10)
+# p2 = Pendulum(1, 0, 10)
+# p1.attach_to(p2)
 
 
 t = 0
-dt = 0.01
+dt = 0.1
 ts = []
-thetas = []
-while t < 10:
-    simple.add_forces(simple.gravity(), simple.tension())
-    simple.update_velocity_and_position(dt)
+p1theta = []
+p1v = []
+p2theta = []
+while t < 1.2:
+    print("_____")
+    print("t = ", t)
     ts.append(t)
-    thetas.append(simple.theta)
+    p1.update_velocity_and_position(dt)
+    p1theta.append(np.rad2deg(p1.theta))
+    p1v.append(np.linalg.norm(p1.velocity))
+
+    # p2.update_velocity_and_position(dt)
+    # p2theta.append(p2.theta)
     t += dt
 
-plt.plot(ts, thetas)
+# plt.plot(ts, p1theta, label="p1")
+plt.plot(ts, p1v, label="p1")
+# plt.plot(ts, p2theta, label="p2")
+plt.legend()
 plt.show()
